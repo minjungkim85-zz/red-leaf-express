d0842dfe187eb02c2eb16188a420b74f82e22d97
kitfoxjw <kitfoxjw@d74d746f-51d8-46ce-b9f4-4ceec8ec905f>
2015-02-21 16:07:28 +0000

    basic physics and prefabs

---
+++ Maple Piercer/Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public Transform target;
+	float defaultCameraSize = 5;
+	float currentCameraSize = 5;
+	
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+		defaultCameraSize = cam.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v =  Vector3.Lerp (transform.position, target.transform.position, 0.99f);
+		v.z = -10;
+		transform.position = v;
+		currentCameraSize -= Input.GetAxis ("Mouse ScrollWheel");
+		if(currentCameraSize < 1) currentCameraSize = 1;
+		cam.orthographicSize = currentCameraSize;
+
+	}
+
+
+}
---
+++ Maple Piercer/Assets/TrainEngine.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainEngine : MonoBehaviour {
+	Rigidbody2D rigidbody;
+	float t = 0;
+	bool countUp = true;
+	public bool isEngine = false;
+	public Vector2 impactDir;
+	float magnitude = 0;
+	public AnimationCurve acceleration;
+	float holdTime = 0;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update(){
+		if (Input.GetMouseButton (0)) {
+			holdTime += Time.deltaTime;
+
+		} else {
+			holdTime -= Time.deltaTime;
+		}
+		holdTime = Mathf.Clamp01 (holdTime);
+	}
+
+	void FixedUpdate(){
+		if(countUp)	t += Time.deltaTime;
+		else t -= Time.deltaTime;
+		rigidbody.AddForce (Vector2.Lerp(Vector2.up * 974, Vector2.up * 980, t));
+		if (countUp && t >= 1) countUp = false;
+		else if(countUp == false && t <= 0) countUp = true;
+
+		if(isEngine && Input.GetMouseButton (0)) rigidbody.AddForce(Vector2.right * acceleration.Evaluate(holdTime) * 2000);
+	}
+
+	void OnGUI(){
+		if(isEngine)GUILayout.Label ("Speed: " + rigidbody.velocity.x);
+	}
+
+	void OnCollisionEnter2D(Collision2D collide){
+		if(collide.collider.attachedRigidbody)
+			collide.collider.attachedRigidbody.AddForce (impactDir * 4, ForceMode2D.Impulse);
+	}
+}
