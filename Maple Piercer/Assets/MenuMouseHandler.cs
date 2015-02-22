1a3910c5197f966ec262399c6d5e2411698f7b1d
skudfisher <skudfisher@d74d746f-51d8-46ce-b9f4-4ceec8ec905f>
2015-02-22 18:55:17 +0000

    main menu

---
+++ Maple Piercer/Assets/MenuMouseHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMouseHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnMouseDown(){
+		if (this.name == "Play"){
+			Application.LoadLevel("main");
+		}
+	}
+}
