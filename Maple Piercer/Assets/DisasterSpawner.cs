35a35e47c9a5e460fd2b6dadfb47dbc8eb001f68
skudfisher <skudfisher@d74d746f-51d8-46ce-b9f4-4ceec8ec905f>
2015-02-22 04:08:42 +0000

    D:

--- Maple Piercer/Assets/DisasterSpawner.cs
+++ Maple Piercer/Assets/DisasterSpawner.cs
@@ -5,16 +5,14 @@ using System.Collections.Generic;
 public class DisasterData{
 	public double pos = 0.0;
 	public int disasterType = 0; 
+
 }
 		
 /***** RAW DATA
 
 	OCCURRENT TYPES
-	1: collision involving track unit
-	2: crossing (e.g., car at crossing)
-	3: dangerous goods leakage
-	4: fire
-	5: trespasser
+	1: crossing (two types)
+	2: trespasser (two types)
 	
 	PROVINCES/TERRITORIES
 	1: AB
@@ -26,38 +24,39 @@ public class DisasterData{
 	7: ON
 	8: QC
 	9: SK
-				1			2			3			4			5
-	1/AB	0.037878788	0.550865801	0.237012987	0.058441558	0.115800866
-	2/BC	0.107923497	0.355191257	0.290983607	0.062841530 0.183060109
-	3/MB	0.037151703	0.603715170	0.191950464	0.055727554	0.111455108
-	4/NB	0.052631579	0.473684211	0.250000000	0.000000000	0.223684211
-	5/NF	0.454545455	0.363636364	0.000000000	0.090909091	0.090909091
-	6/NS	0.000000000	0.656250000	0.093750000	0.000000000	0.250000000
-	7/ON	0.047425474	0.413279133	0.213414634	0.050813008	0.275067751
-	8/QC	0.038142620	0.588723051	0.154228856	0.016583748	0.202321725
-	9/SK	0.030927835	0.734536082	0.097938144	0.067010309	0.069587629
-
  *****/
 
 public class DisasterSpawner : MonoBehaviour {
 	// Matrix of disaster probabilities.  
 	double[][] probAll = new double[][]
 		{	
-			new double[] {0.037878788,0.550865801,0.237012987,0.058441558,0.115800866}, // AB
-			new double[] {0.107923497,0.355191257,0.290983607,0.062841530,0.183060109}, // BC
-			new double[] {0.037151703,0.603715170,0.191950464,0.055727554,0.111455108}, // MB
-			new double[] {0.052631579,0.473684211,0.250000000,0.000000000,0.223684211}, // NB
-			new double[] {0.454545455,0.363636364,0.000000000,0.090909091,0.090909091}, // NF
-			new double[] {0.000000000,0.656250000,0.093750000,0.000000000,0.250000000}, // NS
-			new double[] {0.047425474,0.413279133,0.213414634,0.050813008,0.275067751}, // ON
-			new double[] {0.038142620,0.588723051,0.154228856,0.016583748,0.202321725}, // QC
-			new double[] {0.030927835,0.734536082,0.097938144,0.067010309,0.069587629}  // SK
+		new double[]{0.826298701,0.173701299},
+		new double[]{0.659898477,0.340101523},
+		new double[]{0.844155844,0.155844156},
+		new double[]{0.679245283,0.320754717},
+		new double[]{0.8,	0.2},
+		new double[]{0.724137931,0.275862069},
+		new double[]{0.600393701,0.399606299},
+		new double[]{0.744234801,0.255765199},
+		new double[]{0.913461538,0.086538462}
+		
 		};
 	
 	int nProv, nAcc;				// number of provinces, number of accidents/occurrences.
-	double accProb = .05; 			// gain control for overall probabilities
+	double accProb = .95; 			// gain control for overall probabilities
 	static int nMaxAcc = 1000;  	// maximum possible number of accidents.
 	static double maxJitter = .03;	// maximum amount of position jitter, in proportion of step size.
+	
+	int provID; 					// the province for this run of the game	
+	double trackLength; 			// length of the track in world units.
+	double trackOrigin;				// origin of the track in world units. 
+	
+	public Transform[] prefab;
+	int[][] prefabIDarray = {
+		new int[]{0,1},
+		new int[]{2,3}
+	};
+	int prefabID;
 	
 	/***** DISASTER DATA
 		Let the departure location be 0, and the train destination be 1. Each item in the
@@ -70,7 +69,10 @@ public class DisasterSpawner : MonoBehaviour {
 		nProv = probAll.GetLength (0);
 		nAcc  = probAll[0].GetLength (0);
 		
-		int provID  = 0; // The province for this run of the game. Needs to be passed in somehow.
+		provID  = 1; // The province for this run of the game. Needs to be passed in somehow.
+		
+		trackOrigin = -550; // Origin of the track. We will translate our positions with this. 
+		trackLength = 2250-trackOrigin; // Full length of the track. We will multiply our positions with this. 
 		
 		double[] prob = copyArray(probAll[provID]); // disaster probabilities associated with this province.
 		double[] cProb = cumProb (prob);			// cumulative probabilities for this province. It's easier to determine 
@@ -83,18 +85,27 @@ public class DisasterSpawner : MonoBehaviour {
 				DisasterData d = new DisasterData();
 				
 				// Where does this disaster occur?
-				d.pos = ((double)i)/((double)nMaxAcc) + (Random.Range (-.5f,.5f)*.03 * (1/(double)nMaxAcc));
-				d.pos = Mathf.Min (Mathf.Max (0,(float)d.pos),1);
+				d.pos = ((double)i)/((double)nMaxAcc);// + (Random.Range (-.5f,.5f)*.03 * (1/(double)nMaxAcc)); 
+				d.pos = Mathf.Min (Mathf.Max (0,(float)d.pos),1); // in [0 1]
+				
+				d.pos = d.pos * trackLength; // in [0 trackLength]
+				d.pos = d.pos + trackOrigin; // in [trackOrigin trackLength-trackOrigin];
 				
 				// What type of disaster is it?
 				d.disasterType = findMaxInd (Random.Range (0f,1f),cProb);
 			
 				// Add this disaster to the list. 
 				disasterData.Add(d);
+				
+				// Instantiate.
+				int id = Random.Range (0,prefabIDarray[d.disasterType].GetLength(0));
+				prefabID = prefabIDarray[d.disasterType][id];
+				
+				Instantiate(prefab[prefabID], new Vector3((float)d.pos, 0, 0), Quaternion.identity);
+				
 			}
 		}
-		Debug.Log (disasterData.Count);
-		Debug.Log (dlist2str (disasterData));
+
 		
 	}
 	
