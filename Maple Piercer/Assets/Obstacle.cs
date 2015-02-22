1574063c327a8aa4d9b9fcadacb43b9415965346
kitfoxjw <kitfoxjw@d74d746f-51d8-46ce-b9f4-4ceec8ec905f>
2015-02-22 04:13:58 +0000

    obstacles spawn pieces on destruction

--- Maple Piercer/Assets/Obstacle.cs
+++ Maple Piercer/Assets/Obstacle.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Obstacle : MonoBehaviour {
 	public float damageAmount =20;
 	public bool isDestroyable = true;
+	public GameObject[] pieces;
+	public int pieceSpawnAmount;
 	// Use this for initialization
 	void Start () {
 	
@@ -20,7 +22,20 @@ public class Obstacle : MonoBehaviour {
 
 	void Destroy(){
 		if(isDestroyable) StartCoroutine("DelayedDestroy");
+		if (pieces.Length > 0) {
+			int index = Random.Range(0, pieces.Length);
+			GameObject p = pieces[index] ;
+			for(int i = 0; i < pieceSpawnAmount; i++){
+				GameObject o = Instantiate(p, transform.position + new Vector3(Random.Range(2,3),Random.Range(2,3),0), Quaternion.identity) as GameObject;
+				o.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(0,1),Random.Range(0,1)) * 10 ,ForceMode2D.Impulse);
+			}
+//			foreach(GameObject p in pieces){
+
+//			}
+		}
 	}
+
+
 
 	IEnumerator DelayedDestroy(){
 		yield return new WaitForSeconds (2.0f);
